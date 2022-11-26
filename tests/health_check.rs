2b2f461fc05d9f0159efc42705860e092a3c8660
dubnde <dubnde@gmail.com>
2022-11-26 19:55:58 +0000

    How To Bootstrap A Rust Web API From Scratch - fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,5 @@
 use std::net::TcpListener;
 
-
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -21,11 +20,11 @@ async fn health_check_works() {
 }
 
 fn spawn_app() -> String {
-  let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-  // We retrieve the port assigned to us by the OS
-  let port = listener.local_addr().unwrap().port();
-  let server = zero2prod::run(listener).expect("Failed to bind address");
-  let _ = tokio::spawn(server);
-  // We return the application address to the caller!
-  format!("http://127.0.0.1:{}", port)
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
